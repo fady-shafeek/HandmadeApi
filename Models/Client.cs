bc3e1ced915b626dd92010230ad7b10f20166455
fady-shafeek <Fady.Shafeek@hotmail.com>
2022-03-23 01:10:13 +0200

    Add project files.

---
+++ Models/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace HandmadeApi.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+
+    }
+}
---
+++ Models/Vendor.cs
@@ -0,0 +1,12 @@
+﻿namespace HandmadeApi.Models
+{
+    public class Vendor
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StoreName { get; set; }
+        public string? StoreImg { get; set; }
+
+    }
+}
