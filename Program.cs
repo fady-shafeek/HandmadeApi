910962d04d80341213c4edce3c010cdc592164f1
fady-shafeek <Fady.Shafeek@hotmail.com>
2022-03-24 00:49:54 +0200

    Azure DB

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer("Data Source=.;Initial Catalog=HandmadeDb;Trusted_Connection=True"));
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer("Server=tcp:handmade-db.database.windows.net,1433;Initial Catalog=HandmadeDb;Persist Security Info=False;User ID=handmade;Password=H@ndmade;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
 
 
 var app = builder.Build();
